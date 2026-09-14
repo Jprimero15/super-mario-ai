e87f90dc377ee34ee7a0fdcb93372351c1f2b504
Jprimero15 <jprimero15@aospa.co>
2026-09-14 15:31:51 +0800

    Add C# hazard

---
+++ scripts/Hazard.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public partial class MaryouHazard : Area2D
+{
+    [Signal] public delegate void HitPlayerEventHandler();
+
+    public void Setup(Vector2 size)
+    {
+        CollisionLayer = 0; CollisionMask = 1; Monitoring = true;
+        AddChild(new CollisionShape2D { Shape = new RectangleShape2D { Size = size } });
+        BodyEntered += OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is MaryouPlayer) EmitSignal(SignalName.HitPlayer);
+    }
+}
