4a33d9051993c34b29bcb235abf119607cf2e7a6
Jprimero15 <jprimero15@aospa.co>
2026-09-14 11:08:43 +0800

    feat: add vector shield power-up entity

---
+++ core/src/main/kotlin/com/yourgame/mario/entities/ShieldOrb.kt
@@ -0,0 +1,4 @@
+package com.yourgame.mario.entities
+
+/** Collectible shield that grants temporary damage immunity. */
+class ShieldOrb(x: Float, y: Float, width: Float = 22f, height: Float = 22f) : Entity(x, y, width, height)
