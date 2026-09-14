70c830f8b476633b5137a6f5317115862fea381b
Jprimero15 <jprimero15@aospa.co>
2026-09-15 00:52:56 +0800

    Remove incorrect hazard group from checkpoints

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -4,7 +4,6 @@ signal checkpoint_reached(position: Vector2)
 var active := false
 
 func _ready() -> void:
-	add_to_group("hazards")
 	collision_layer = 0
 	collision_mask = 1
 	body_entered.connect(_on_body_entered)
