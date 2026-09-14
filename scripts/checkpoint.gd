958e1c377acd0b9bb7f9ae296078c8cbd330a968
Jprimero15 <jprimero15@aospa.co>
2026-09-15 05:12:31 +0800

    Wire checkpoint signal and state handling

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+class_name MaryouCheckpoint
+
 signal checkpoint_reached(position: Vector2)
 var active := false
 
 func _ready() -> void:
 	collision_layer = 0
 	collision_mask = 1
+	monitoring = true
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
