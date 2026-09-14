ceeb522bace10e988143a756213e496b1579ce5f
Jprimero15 <jprimero15@aospa.co>
2026-09-15 00:51:56 +0800

    Remove score formula and make steps the sole score

--- scripts/score_manager.gd
+++ scripts/score_manager.gd
@@ -2,8 +2,7 @@ extends Node
 
 var steps := 0
 var coins := 0
-var combo := 0
-var best_score := 0
+var best_steps := 0
 var run_active := false
 
 func _ready() -> void:
@@ -12,39 +11,31 @@ func _ready() -> void:
 func reset_run() -> void:
 	steps = 0
 	coins = 0
-	combo = 0
 	run_active = true
 
 func add_coin() -> void:
 	coins += 1
-	combo += 1
 	if has_node("/root/AudioManager"): AudioManager.play_sfx("coin")
 
 func add_stomp() -> void:
-	combo += 1
 	if has_node("/root/AudioManager"): AudioManager.play_sfx("stomp")
-
-func multiplier() -> int:
-	return clampi(1 + int(combo / 5), 1, 9)
-
-func score() -> int:
-	return steps * 5 + coins * 25 * multiplier() + combo * 10
 
 func finish_run() -> void:
 	if not run_active: return
 	run_active = false
-	best_score = maxi(best_score, score())
+	best_steps = maxi(best_steps, steps)
 	save_best()
 	if has_node("/root/AudioManager"): AudioManager.play_sfx("game_over")
 
 func load_best() -> void:
 	var cfg := ConfigFile.new()
 	if cfg.load("user://records.cfg") != OK:
-		best_score = 0
+		best_steps = 0
 		return
-	best_score = maxi(0, int(cfg.get_value("records", "best_score", 0)))
+	best_steps = maxi(0, int(cfg.get_value("records", "best_steps", 0)))
 
 func save_best() -> void:
 	var cfg := ConfigFile.new()
-	cfg.set_value("records", "best_score", best_score)
-	if cfg.save("user://records.cfg") != OK: push_warning("Could not save records.cfg")
+	cfg.set_value("records", "best_steps", best_steps)
+	if cfg.save("user://records.cfg") != OK:
+		push_warning("Could not save records.cfg")
