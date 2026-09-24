d460f9f92cb61d163b4487460245e5d2e491d5fb
Jprimero15 <jprimero15@aospa.co>
2026-09-24 11:29:03 +0800

    Add persistent run and coin progression

--- scripts/score_manager.gd
+++ scripts/score_manager.gd
@@ -3,18 +3,24 @@ extends Node
 var steps := 0
 var coins := 0
 var best_steps := 0
+var total_coins := 0
+var total_runs := 0
 var run_active := false
+var new_best := false
 
 func _ready() -> void:
-	load_best()
+	load_records()
 
 func reset_run() -> void:
 	steps = 0
 	coins = 0
+	new_best = false
 	run_active = true
 
 func add_coin() -> void:
 	coins += 1
+	total_coins += 1
+	save_records()
 	AudioManager.play_sfx("coin")
 
 func add_stomp() -> void:
@@ -23,19 +29,27 @@ func add_stomp() -> void:
 func finish_run() -> void:
 	if not run_active: return
 	run_active = false
+	total_runs += 1
+	new_best = steps > best_steps
 	best_steps = maxi(best_steps, steps)
-	save_best()
+	save_records()
 	AudioManager.play_sfx("game_over")
 
-func load_best() -> void:
+func load_records() -> void:
 	var cfg := ConfigFile.new()
 	if cfg.load("user://records.cfg") != OK:
 		best_steps = 0
+		total_coins = 0
+		total_runs = 0
 		return
 	best_steps = maxi(0, int(cfg.get_value("records", "best_steps", 0)))
+	total_coins = maxi(0, int(cfg.get_value("records", "total_coins", 0)))
+	total_runs = maxi(0, int(cfg.get_value("records", "total_runs", 0)))
 
-func save_best() -> void:
+func save_records() -> void:
 	var cfg := ConfigFile.new()
 	cfg.set_value("records", "best_steps", best_steps)
+	cfg.set_value("records", "total_coins", total_coins)
+	cfg.set_value("records", "total_runs", total_runs)
 	if cfg.save("user://records.cfg") != OK:
 		push_warning("Could not save records.cfg")
